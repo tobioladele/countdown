c8c2dd9029d1f44da765fa2a0883c86951a979f9
Tobi oladele <tedlade.gmail.com>
2016-09-27 01:42:39 +0100

    Added comment for readability

--- js/app.js
+++ js/app.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
     var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date(Date.parse(new Date()) + 30 * 24 * 60 * 60 * 1000);
+var deadline = new Date(Date.parse(new Date()) + 30 * 24 * 60 * 60 * 1000); //Days, Hours, Minutes, Seconds 
 initializeClock('clockdiv', deadline);
